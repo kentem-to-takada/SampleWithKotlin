f0485a9f25191982a430c93b3de8fc474176c194
kentem-to-takada <to-takada@kentem.co.jp>
2024-12-19 10:13:03 +0900

    バージョンUPと設定内容の変更

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,13 @@
+/**
+ * @type {import("prettier").Config}
+ */
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  semi: true,
+  endOfLine: 'lf',
+  printWidth: 120,
 };
